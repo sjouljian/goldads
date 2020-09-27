08e70c8dd63802bfd2b7c148b2e66753c519bdb2
sjouljian <45968865+sjouljian@users.noreply.github.com>
2020-09-27 16:59:22 +0300

    .

--- faq.php
+++ faq.php
@@ -28,26 +28,26 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 col-xs1-12" style="color:#007c88;"></div>
-            <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-xs1-12" style="color:#007c88;">
-           <h4 class="text-dark">1-What is <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span> Pack?</h4>
-           <p ><span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> Is a Paid to Click Advertising site where you can show your preferred Affiliate websites to other members of<br> <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> while earning up to 130% .</p>
-           <h4  class="text-dark">2-How long does Gold Ads Pack run for?</h4>
+            <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-xs1-12" style="color:#2e3b42">
+           <h4 class="mt-5 text-dark">1-What is <span >Gold</span> <span >Ads</span> Pack?</h4>
+           <p ><span>Gold</span> <span>Ads</span><span>Pack</span> Is a Paid to Click Advertising site where you can show your preferred Affiliate websites to other members of<br> <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> while earning up to 130% .</p>
+           <h4  class="mt-5 text-black">2-How long does Gold Ads Pack run for?</h4>
            <p>Each AdPack runs for 30 Days. After this period it will expire. Every purchase is handled separately.</p>
-           <h4  class="text-dark">3-How many visit per AdPack will I get?</h4>
+           <h4  class="mt-5 text-black">3-How many visit per AdPack will I get?</h4>
            <p>You will get 100 GUARANTEED Visits per Ads Pack.</p>
-           <h4  class="text-dark">4-How many websites should I visit to earn 130% back?</h4>
+           <h4  class="mt-5 text-black">4-How many websites should I visit to earn 130% back?</h4>
            <p>You should visit 10 websites every day for 30 Days.</p>
-           <h4  class="text-dark">5-Can I earn commission as a Free member?</h4>
+           <h4  class="mt-5 text-black">5-Can I earn commission as a Free member?</h4>
            <p>Yes. We offer a two tiers Affiliate programs. Your direct referrals will earn you 7% of their purchases made with a payment processor and 3% of their repurchases. Secon tier referrals (referrals of your referrals) will earn you 3% of their purchases made with a payment processor and 1% of their repurchases.</p>
-           <h4  class="text-dark">6-Can I earn Credits to show my websites?</h4>
+           <h4  class="mt-5 text-black">6-Can I earn Credits to show my websites?</h4>
            <p>Yes. You earn Credits by visiting other members websites.</p>
-           <h4  class="text-dark">7-When do You pay Commissions?</h4>
+           <h4  class="mt-5 text-black">7-When do You pay Commissions?</h4>
            <p>When you reach the treshold of the minimum required to cashout you will be allowed to request a withdraw from the Withdrawal page. We pay out with the "as soon as possible" formula, we have no reason to keep the money in our accounts but we pay only 1 payout request per person at a time!</p>
-           <h4  class="text-dark">8-Can I join and stay as a Free member?</h4>
+           <h4  class="mt-5 text-black">8-Can I join and stay as a Free member?</h4>
            <p>Yes, You can Join and stay as a Free member forever. there is absolutely no obligation to make any purchase.</p>
-           <h4  class="text-dark">9-Can I have more than one account?</h4>
+           <h4  class="mt-5 text-black">9-Can I have more than one account?</h4>
            <p>Absolutely NOT. There is no need to maintain more than one account. If you've lost your login information, you can request to have it sent to your e-mail address. If you are caught with more then one account open, your account(s) will be terminated, your ip address will be banned and you will not receive any earnings.</p>
-           <h4  class="text-dark">10-My question is not on this list..</h4>
+           <h4  class="mt-5 text-black">10-My question is not on this list..</h4>
            <p>Please contact us by clicking on the link at the bottom of this page.</p>
             </div>
             <div class="col-xl-2 col-lg-2 " style="color:#007c88;"></div>
@@ -58,7 +58,8 @@
 <div class="container-fluid">
     <div class="container">
         <div class="row reg">
- <a href="index.html#slider" type="submit" style="background-color: #007c88; " class=" mb-5 btn2 font-weight-bold text-light px-3" >Create your Account</a>
+ <a href="index.php#slider" class="mx-auto" >
+ <button class="mb-5 btn2 btn btn-submit font-weight-bold text-white px-3">Create your Account</button></a>
         </div>
     </div>
 </div>
--- rules.php
+++ rules.php
@@ -29,28 +29,26 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 col-xs1-12" style="color:#007c88;"></div>
-            <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-xs1-12 mt-55" style="color:#007c88;">
-            <p>The following Terms and Conditions control your membership in <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> Service.
-                 You agree that you have read and understand this Agreement and that your membership in <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> shall be subject to the following Terms and Conditions between you Gold Ads Pack.
-                  These Terms and Conditions may be modified at any time by <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span>. Please review them from time to time since your ongoing use is subject to the terms and conditions as modified.
-                   Your continued participation in <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> after such modification shall be deemed to be your acceptance of any such modification. If you do not agree to these Terms and Conditions,
-                   please do not register to become a member of <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span></p>
-                    <h5>Registration</h5>
+            <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-xs1-12 mt-55" style="color:#2e3b42">
+            <p>The following Terms and Conditions control your membership in <span >Gold</span> <span >Ads</span><span >Pack</span> Service.
+                 You agree that you have read and understand this Agreement and that your membership in <span >Gold</span> <span >Ads</span><span >Pack</span> shall be subject to the following Terms and Conditions between you Gold Ads Pack.
+                  These Terms and Conditions may be modified at any time by <span >Gold</span> <span >Ads</span><span >Pack</span>. Please review them from time to time since your ongoing use is subject to the terms and conditions as modified.
+                   Your continued participation in <span >Gold</span> <span >Ads</span><span >Pack</span> after such modification shall be deemed to be your acceptance of any such modification. If you do not agree to these Terms and Conditions,
+                   please do not register to become a member of <span >Gold</span> <span >Ads</span><span >Pack</span></p>
+                    <h5 class="mt-5 text-black">Registration</h5>
                     <p>In order to participate in the program you should Register an account and verify your email. After registering you will receive an email with a verification link that you should click in order to get your account verified.</p>
-                <h5>Purchasing <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span></h5>
-                <p>Simply click on the "Purchase" <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> button to make a purchase. The process is straightforward, just decide which and how many adpack you want and follow instructions.</p>
-            <h5>Maximum Purchase</h5>
+                <h5 class="mt-5 text-black">Purchasing <span >Gold</span> <span >Ads</span><span >Pack</span></h5>
+                <p>Simply click on the "Purchase" <span >Gold</span> <span >Ads</span><span >Pack</span> button to make a purchase. The process is straightforward, just decide which and how many adpack you want and follow instructions.</p>
+            <h5 class="mt-5 text-black">Maximum Purchase</h5>
                 <p>Currenlty, the maximum Active Ads Pack purchase is 100 AdPacks a month </p>
-            <h5><span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> and earnings</h5>
-            <br>
-            <h5>Advertising</h5>
-            <br>
-            <h5>Affiliate Commissions</h5>
+            <h5 class="mt-5 text-black"><span >Gold</span> <span >Ads</span><span >Pack</span> and earnings</h5>
+            <h5 class="mt-5 text-black">Advertising</h5>
+            <h5 class="mt-5 text-black">Affiliate Commissions</h5>
             <p>We offer Affiliate program. Your referrals will earn you 10% of their purchases.</p>
-        <h5>Withdrawals</h5>
+        <h5 class="mt-5 text-black">Withdrawals</h5>
         <p>When you reach the treshold of the minimum required to cashout you will be allowed to request a withdraw from the Withdrawal page. We pay out with-in 3 working days,</p>
             
-                <span style="color: goldenrod;">Gold</span> <span style="color: red;">Ads</span><span style="color:black;">Pack</span> Team</p>
+                <span >Gold</span> <span >Ads</span><span >Pack</span> Team</p>
         </div>
             <div class="col-xl-2 col-lg-2 " style="color:#007c88;"></div>
         </div>
@@ -60,7 +58,8 @@
 <div class="container-fluid">
     <div class="container">
         <div class="row reg">
- <a href="index.html#slider" type="submit" style="background-color: #007c88; " class=" mb-5 btn2 font-weight-bold text-light px-3" >Create your Account</a>
+        <a href="index.php#slider" class="mx-auto" >
+ <button class="mb-5 btn2 btn btn-submit font-weight-bold text-white px-3">Create your Account</button></a>
         </div>
     </div>
 </div>
